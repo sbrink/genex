cb64dbfd1b366081a48e79a184285daf63d28f6f
Sascha Brink <sascha.brink@gmail.com>
2019-01-26 20:58:55 +0100

    Initial commit

---
+++ lib/genex.ex
@@ -0,0 +1,18 @@
+defmodule Genex do
+  @moduledoc """
+  Documentation for Genex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Genex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
